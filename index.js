5bdd23ce6f8389b0246557dea889af809b55a818
Code-With-Fred <ezefavourchimereze@gmail.com>
2025-05-12 16:32:38 +0100

    work

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const Auth = require("./authModel")
 const dotenv = require("dotenv").config()
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken");
 
 const app = express()
 app.use(express.json())
@@ -34,10 +35,52 @@ app.post("/sign-up", async (req, res) => {
 
 
     const newUser = new Auth({
-        firstName, lastName, email, password:hashedPassword, state
+        firstName, lastName, email, password: hashedPassword, state
     })
     await newUser.save()
-    res.json({ message: "User created successfully", newUser:firstName, lastName, email, state});
+    res.json({ message: "User created successfully", newUser: firstName, lastName, email, state });
 
+
+})
+// login day
+app.post("/login", async (req, res)=>{
+    const {email, password} = req.body
+    const User = await Auth.findOne({email})
+    if(!User){
+        return res.status(404).json({message:"user not found"})
+    }
+    const isMatch = await bcrypt.compare(password, User?.password)
+    if(!isMatch){
+        return res.status(400).json({message:"incorrrect email or pasasswor"})
+    }
+    // generate token
+    const accessToken = jwt.sign(
+        {User},
+        process.env.ACCESSTOKEN,
+        {expiresIn:"3d"}
+    )
+    const refreshToken = jwt.sign(
+        {User},
+        process.env.REFRENCETOKEN,
+        {expiresIn:"30d"})
+    res.status(200).json({
+        message:"login successful",
+        accessToken,
+        user:{
+            email:User?.email,
+            firstName:User?.firstName,
+            lastName:User?.lastName,
+            state:User?.state
+        }
+    })
+})
+
+app.post("/forgot-password", async (req, res) =>{
+    const {email} = req.body
+    const User = await Auth.findOne({email})
+    if(!User){
+        return res.status(404).json({message:"user not found"})
+    }
+    res.status(200).json({message:"please check email"})
 
 })
